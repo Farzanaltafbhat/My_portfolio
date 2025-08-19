fca1ff8834112c704ab812dab36c99265b399f75
Farzanaltafbhat <farzanbhat231@gmail.com>
2025-08-19 15:37:49 +0530

    solved footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import "./Footer.css";
 import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
 const Footer: React.FC = () => {
   return (
     <footer className="footer-section">
       <div className="container-fluid text-center py-4">
         {/* Navigation Links */}
         <ul className="footer-links mb-3">
-          <li><a href="/" className="footer-link">Home</a></li>
-          <li><a href="/projects" className="footer-link">Projects</a></li>
-          <li><a href="/about" className="footer-link">About</a></li>
-          <li><a href="/contact" className="footer-link">Contact</a></li>
+          <li><Link to="/" className="footer-link">Home</Link></li>
+<li><Link to="/projects" className="footer-link">Projects</Link></li>
+<li><Link to="/about" className="footer-link">About</Link></li>
+<li><Link to="/contact" className="footer-link">Contact</Link></li>
         </ul>
 
         {/* Social Icons */}
         <div className="social-icons mb-3">
           <a href="https://github.com/Farzanaltafbhat" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-          <a href="www.linkedin.com/in/farzan-altaf-bhat-a6b358245" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          <a href="https://www.linkedin.com/in/farzan-altaf-bhat-a6b358245" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
           <a href="https://wa.me/+917780820718" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
         </div>
 
