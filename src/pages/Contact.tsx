c9c68a7abc34b9fc1e8f8cb839a671c8c6763c0c
Farzanaltafbhat <farzanbhat231@gmail.com>
2025-08-19 15:59:54 +0530

    added render api

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -19,7 +19,7 @@ const Contact: React.FC = () => {
     setStatus("Sending...");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://portfolio-backend-dpg9.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
